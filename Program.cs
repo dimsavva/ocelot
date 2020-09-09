6346a4b8cfa779d17ec4d4e4118dd9ef4a1546fc
Jimmy Savva <jimmy.savva@entelect.co.za>
2020-09-09 19:30:14 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                   .ConfigureAppConfiguration((hostingContext, config) =>
+                   {
+                       config
+                           .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                           .AddJsonFile("ocelot.json", true, true)
+                           .AddEnvironmentVariables();
+                   })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
